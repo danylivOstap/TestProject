bdd31c42f9b3b3462de817fbe81d4cd879f4cbba
Ostap <danyliv.ostap@gmail.com>
2021-12-09 23:51:54 +0200

    added methods sum() and div()

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+public class App {
+
+    public static void Main(String[] Args){
+        int a = 10;
+        int b = 7;
+        System.out.println(sum(a, b));
+        System.out.println(div(a, b));
+    }
+
+    public static int sum(int a, int b){
+        return a+b;
+    }
+
+    public static int div(int a, int b){
+        return a - b;
+    }
+
+}
